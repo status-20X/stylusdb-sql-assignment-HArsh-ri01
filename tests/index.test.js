21227ef81140a28224ebec103c64023cc14abfcf
Harshit Gunjal <21f1003624@ds.study.iitm.ac.in>
2024-04-12 21:02:30 +0530

    'test_2'

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,14 @@
 test('Basic Jest Test', () => {
     expect(1).toBe(1);
   });
+
+
+const readCSV = require('../src/csvReader');
+
+test('Read CSV file', async () => {
+  const data = await readCSV('./sample.csv');
+  expect(data.length).toBEGreaterThan(0);
+  expect(data.length).toBe(3);
+  expect(data[0].name).toBe('John');
+  expect(data[0].age).toBe('30');
+})
